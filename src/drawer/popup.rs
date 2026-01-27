835bedeeb8f8bdddae1d37257a1bfa863edd4b21
undefinedpatient <undefinedpatient@gmail.com>
2026-01-27 15:30:47 +0800

    Updated Better MessagePopup allowing size and color;

--- src/drawer/popup.rs
+++ src/drawer/popup.rs
@@ -8,7 +8,10 @@ use ratatui::widgets::{Block, BorderType, Clear, Padding, Paragraph, Widget, Wra
 use ratatui::Frame;
 impl Drawable for MessagePopup {
     fn render(&self, frame: &mut Frame, area: Rect, draw_flag: DrawFlag) {
-        let popup_area: Rect = area.centered(Constraint::Length(42), Constraint::Length(6));
+        let ratio: f64 = 1.0/2.0;
+        let width: u16 =  (((self.message.len().isqrt() as f64 + 1f64)  / ratio) as u16).clamp(42, area.width)+6;
+        let height: u16 =  (((self.message.len().isqrt() as f64 + 1f64) * ratio) as u16).clamp(6, area.height)+6;
+        let popup_area: Rect = area.centered(Constraint::Length(width), Constraint::Length(height));
         let paragraph_message: Paragraph = Paragraph::new(self.message.clone())
             .wrap(Wrap { trim: true })
             .alignment(Alignment::Center)
@@ -16,15 +19,16 @@ impl Drawable for MessagePopup {
                 Block::bordered()
                     .border_type(BorderType::Double)
                     .padding(Padding::uniform(1))
-                    .title_top(Line::from("Message").centered())
-                    .title_bottom(Line::from(Span::from("[Understood]").bold()).centered())
+                    .title_top(Line::from(" Message ").centered())
+                    .title_bottom(Line::from(Span::from(" [Understood] ").bold()).centered())
+                    .style(self.color)
             } else {
                 Block::bordered()
                     .padding(Padding::uniform(1))
-                    .title_top(Line::from("Message").centered())
-                    .title_bottom(Line::from(Span::from("[Understood]").bold()).centered())
+                    .title_top(Line::from(" Message ").centered())
+                    .title_bottom(Line::from(Span::from(" [Understood] ").bold()).centered())
+                    .style(self.color)
             });
-
         Clear.render(popup_area, frame.buffer_mut());
         paragraph_message.render(popup_area, frame.buffer_mut());
     }
